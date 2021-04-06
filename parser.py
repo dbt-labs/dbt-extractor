124b3e23e63bca03ccbed7137c19cbc0cdcdb530
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-04-06 10:06:31 -0400

    bail parsing on first error, scope mutations

--- parser.py
+++ parser.py
@@ -30,39 +30,58 @@ def strip_quotes(text):
         return text[1:-1]
 
 def extract_refs(string, node, data):
-    if node.type == 'dbt_jinja_ref':
-        package_name = node.child_by_field_name('dbt_package_name')
-        model_name = node.child_by_field_name('dbt_model_name')
-        ref = (
-            strip_quotes(text_at(string, package_name)),
-            strip_quotes(text_at(string, model_name)),
-        )
+    # local mutation to avoid mutating the input
+    data2 = dict(data)
+    # flag so we can bail when an error is found
+    no_error = True
 
-        data['refs'].add(ref)
-    if node.type == 'dbt_jinja_source':
+    # if this limited tree-sitter implementaion can't parse it, python jinja will have to
+    if node.type == 'ERROR':
+        no_error = False
+        data2['python_jinja'] += 1
+
+    elif node.type == 'dbt_jinja_ref':
+        package_name = node.child_by_field_name('dbt_package_name')
+        model_name = node.child_by_field_name('dbt_model_name') 
+        fmodel_name = strip_quotes(text_at(string, model_name))
+        if not package_name:
+            ref = fmodel_name
+        else:
+            ref = (
+                strip_quotes(text_at(string, package_name)),
+                fmodel_name
+            )
+        data2['refs'].add(ref)
+
+    elif node.type == 'dbt_jinja_source':
         source_name = node.child_by_field_name('dbt_source_name')
         table_name = node.child_by_field_name('dbt_source_table')
         source = (
             strip_quotes(text_at(string, source_name)),
-            strip_quotes(text_at(string, table_name)),
+            strip_quotes(text_at(string, table_name))
         )
+        data2['sources'].add(source)
 
-        data['sources'].add(source)
-    if node.type == 'dbt_jinja_config':
+    elif node.type == 'dbt_jinja_config':
         config_nodes = [child for child in node.children if child.type == 'kwarg_expression']
         for config in config_nodes:
-            arg, _, value = config.children
+            try:
+                arg, _, value = config.children
+            except Exception as e:
+                print('----------')
+                print(config.children)
+                print('----------')
+                raise e
 
             arg_val = text_at(string, arg)
             val_val = strip_quotes(text_at(string, value))
-            data['configs'][arg_val] = val_val
+            data2['configs'][arg_val] = val_val
 
-    # if this limited tree-sitter implementaion can't parse it, python jinja will have to
-    if node.type == 'ERROR':
-        data['python_jinja'] += 1
+    if no_error:
+        for child in node.children:
+            extract_refs(string, child, data2)
 
-    for child in node.children:
-        extract_refs(string, child, data)
+    return data2
 
 def get_parser():
     parser = Parser()
@@ -78,5 +97,4 @@ def parse_string(parser, string):
         'configs': dict(),
         'python_jinja': 0
     }
-    extract_refs(string, tree.root_node, data)
-    return data
+    return extract_refs(string, tree.root_node, data)
