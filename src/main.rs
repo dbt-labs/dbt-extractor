1c5a7ae61dd965d547a644c20f80ab031c578c48
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-06-08 13:18:18 -0400

    attempt to use language with tree-sitter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let code = "This text supports {{ 'jinja' }}.";
+    let mut parser = tree_sitter::Parser::new();
+    parser.set_language(tree_sitter_jinja2::language()).expect("Error loading jinja2 grammar");
+    let tree = parser.parse(code, None).unwrap();
+    let root_node = tree.root_node();
+
+    assert_eq!(root_node.kind(), "source_file");
+    println!("code parsed.");
 }
