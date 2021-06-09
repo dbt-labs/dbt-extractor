c723f5682bb001b372f2897071f17f51cb8ceb95
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-06-09 14:58:35 -0400

    introduce ExprU

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -2,31 +2,55 @@ use std::fmt;
 use std::fmt::Display;
 use std::str::Utf8Error;
 
+
+// Top-level error type in the hierarchy
 #[derive(Debug, Clone)]
-pub enum TypeError {
+pub enum ParseError {
+    SourceE(SourceError),
+    TypeE(TypeError),
+}
+
+// TODO use crate `thiserror` to simplify boilerplate
+#[derive(Debug, Clone)]
+pub enum SourceError {
     BadSourceExtraction,
     Utf8Err(Utf8Error),
     BadBoolean(String),
     UnknownNodeType(String),
-    BadAssignment(String, String),
     MissingValue(String, String),
+}
+
+impl Display for SourceError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SourceError::BadSourceExtraction =>
+                write!(f, "BadSourceExtraction"),
+            SourceError::Utf8Err(e) =>
+                e.fmt(f),
+            SourceError::BadBoolean(s) =>
+                write!(f, "Unknown Boolean value: {}", s),
+            SourceError::UnknownNodeType(s) =>
+                write!(f, "Unknown node type: {}", s),
+            SourceError::MissingValue(outer, inner) =>
+                write!(f, "{} is missing the required value {}", outer, inner),
+        }
+    }
+}
+
+// TODO use crate `thiserror` to simplify boilerplate
+#[derive(Debug, Clone)]
+pub enum TypeError {
+    BadAssignment(String, String),
+    KwargsAreNotLast,
 }
 
 impl Display for TypeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            TypeError::BadSourceExtraction =>
-                write!(f, "BadSourceExtraction"),
-            TypeError::Utf8Err(e) =>
-                e.fmt(f),
-            TypeError::BadBoolean(s) =>
-                write!(f, "Unknown Boolean value: {}", s),
-            TypeError::UnknownNodeType(s) =>
-                write!(f, "Unknown node type: {}", s),
             TypeError::BadAssignment(outer, inner) =>
                 write!(f, "{} cannot be assigned a {}", outer, inner),
-            TypeError::MissingValue(outer, inner) =>
-                write!(f, "{} is missing the required value {}", outer, inner),
+            TypeError::KwargsAreNotLast =>
+                write!(f, "Keyword arguments must come at the end of the argument list."),
         }
     }
 }
--- src/extractor.rs
+++ src/extractor.rs
@@ -1,4 +1,8 @@
-use crate::exceptions::TypeError;
+use crate::exceptions::{
+    ParseError,
+    SourceError,
+    TypeError,
+};
 use std::collections::HashMap;
 use std::str::{
     from_utf8,
@@ -8,6 +12,7 @@ use tree_sitter::{
     Tree,
 };
 
+// final result
 #[derive(Clone, Debug)]
 pub struct Extraction {
     refs: Vec<String>,
@@ -15,24 +20,51 @@ pub struct Extraction {
     configs: Vec<HashMap<String, String>>,
 }
 
-// typed ast data type
-// TODO model kwargs
+// untyped ast
+#[derive(Clone, Debug)]
+enum ExprU {
+    RootU(Vec<ExprU>),
+    StringU(String),
+    BoolU(bool),
+    ListU(Vec<ExprU>),
+    DictU(HashMap<String, ExprU>),
+    KwargU(Box<ExprU>, Box<ExprU>),
+    // built-in function types
+    FnCallU(String, Vec<ExprU>),
+}
+
+// typed ast
 #[derive(Clone, Debug)]
 enum ExprT {
+    RootT(Vec<ExprT>),
     StringT(String),
     BoolT(bool),
     // lists are heterogeneous
     ListT(Vec<ExprT>),
     DictT(HashMap<String, ExprT>),
-    // all represented as positional arguments regardless of kwargs in source
-    FnCall(String, Vec<ExprT>),
-    Root(Vec<ExprT>),
+    // built-in function types
+    // args represented as positional regardless of kwargs in source
+    RefT(Vec<ExprU>),
+    SourceT(Vec<ExprU>),
+    ConfigT(Vec<ExprU>),
 }
 
-fn text_from_node<'a>(source: &'a Vec<u8>, node: &Node) -> Result<&'a str, TypeError> {
+fn map_err<A, E1, E2>(r: Result<A, E1>, f: fn(E1) -> E2) -> Result<A, E2> {
+    match r {
+        Ok(a)  => Ok(a),
+        Err(e) => Err(f(e)),
+    }
+}
+
+fn child_by_field_name<'a, 'b>(node: &'a Node, name: &'b str) -> Result<Node<'a>, SourceError> {
+    node.child_by_field_name(name)
+        .ok_or(SourceError::MissingValue(node.kind().to_owned(), name.to_owned()))
+}
+
+fn text_from_node<'a>(source: &'a Vec<u8>, node: &Node) -> Result<&'a str, SourceError> {
     match from_utf8(&source[node.start_byte()..node.end_byte()]) {
         Ok(s) => Ok(s),
-        Err(e) => Err(TypeError::Utf8Err(e)),
+        Err(e) => Err(SourceError::Utf8Err(e)),
     }
 }
 
@@ -44,61 +76,42 @@ fn strip_first_and_last(s: &str) -> String {
     }
 }
 
-fn named_children(node: Node) -> Vec<Node> {
-    // calling allocating a new cursor every time is less effecient, but juggling lifetimes also sucks.
-    node.named_children(&mut node.walk()).collect()
-}
-
-fn child_by_field_name<'a, 'b>(node: &'a Node, name: &'b str) -> Result<Node<'a>, TypeError> {
-    node.child_by_field_name(name)
-        .ok_or(TypeError::MissingValue(node.kind().to_owned(), name.to_owned()))
-}
-
-fn type_check(source_bytes: &Vec<u8>, tree: Tree) -> Result<ExprT, TypeError> {
-    _type_check(source_bytes, tree.root_node())
-}
-
-fn _type_check(source: &Vec<u8>, node: Node) -> Result<ExprT, TypeError> {
+fn to_ast(source: &Vec<u8>, node: Node) -> Result<ExprU, SourceError> {
     let kind = node.kind();
     match kind {
         "source_file" => {
-            let x: Result<Vec<ExprT>, TypeError> = 
+            let x: Result<Vec<ExprU>, SourceError> = 
             named_children(node)
                     .into_iter()
-                    .map(|child| _type_check(source, child))
+                    .map(|child| to_ast(source, child))
                     .collect();
 
             match x {
-                Ok(children) => Ok(ExprT::Root(children)),
-                Err(_)       => Err(TypeError::BadSourceExtraction),
+                Ok(children) => Ok(ExprU::RootU(children)),
+                Err(_)       => Err(SourceError::BadSourceExtraction),
             }
         },
 
         "lit_string" => {
             match text_from_node(&source, &node) {
-                Ok(s)  => Ok(ExprT::StringT(strip_first_and_last(s))),
-                Err(_) => Err(TypeError::BadSourceExtraction),
+                Ok(s)  => Ok(ExprU::StringU(strip_first_and_last(s))),
+                Err(_) => Err(SourceError::BadSourceExtraction),
             }
         },
 
         "bool" => {
             match text_from_node(&source, &node) {
-                Ok("True")  => Ok(ExprT::BoolT(true)),
-                Ok("False") => Ok(ExprT::BoolT(false)),
-                Ok(s)       => Err(TypeError::BadBoolean(s.to_owned())),
-                Err(_)      => Err(TypeError::BadSourceExtraction),
+                Ok("True")  => Ok(ExprU::BoolU(true)),
+                Ok("False") => Ok(ExprU::BoolU(false)),
+                Ok(s)       => Err(SourceError::BadBoolean(s.to_owned())),
+                Err(_)      => Err(SourceError::BadSourceExtraction),
             }
         },
 
-        // Throw away names. 
-        // In the step before getting here, names will be checked for correctness.
         "kwarg" => {
+            let key = text_from_node(source, &child_by_field_name(&node, "key")?)?;
             let value = child_by_field_name(&node, "value")?;
-            match value.kind() {
-                "fn_call" => Err(TypeError::BadAssignment("kwarg".to_owned(), "fn_call".to_owned())),
-                // No intermediary kwarg type. Just return the underlying value.
-                _         => _type_check(source, value),
-            }
+            Ok(ExprU::KwargU(Box::new(ExprU::StringU(key.to_owned())), Box::new(to_ast(source, value)?)))
         },
 
         "dict" => {
@@ -107,37 +120,150 @@ fn _type_check(source: &Vec<u8>, node: Node) -> Result<ExprT, TypeError> {
                 let key_node = child_by_field_name(&pair, "key")?;
                 let key = strip_first_and_last(text_from_node(&source, &key_node)?);
                 let value_node = child_by_field_name(&pair, "key")?;
-                let value = _type_check(source, value_node)?;
+                let value = to_ast(source, value_node)?;
 
                 dict.insert(key, value);
             }
-            // TODO stub
-            Ok(ExprT::DictT(dict))
+            Ok(ExprU::DictU(dict))
         },
 
         "list" => {
             let mut list = vec![];
             for elem in named_children(node) {
-                if elem.kind() == "fn_call" {
-                    return Err(TypeError::BadAssignment("list element".to_owned(), "fn_call".to_owned()))
-                }
-                let e = _type_check(source, elem)?;
-                list.push(e);
+                list.push(to_ast(source, elem)?);
             }
-            Ok(ExprT::ListT(list))
+            Ok(ExprU::ListU(list))
         },
 
-        s => Err(TypeError::UnknownNodeType(s.to_owned()))
+        "fn_call" => {
+            let name = text_from_node(source, &child_by_field_name(&node, "fn_name")?)?;
+            let arg_list = child_by_field_name(&node, "argument_list")?;
+            let mut args = vec![];
+            for arg in named_children(arg_list) {
+                args.push(to_ast(source, arg)?)
+            }
+            Ok(ExprU::FnCallU(name.to_owned(), args))
+        },
+
+        s => Err(SourceError::UnknownNodeType(s.to_owned()))
     }
 }
 
-pub fn extract_from_source(source: &str) -> Result<Extraction, TypeError> {
+fn kwargs_last(args: &Vec<Node>) -> bool {
+    let mut seen_kwarg = false;
+    for arg in args {
+        if seen_kwarg && arg.kind() != "kwarg" {
+            return false
+        } else if arg.kind() == "kwarg" {
+            seen_kwarg = true;
+        }
+    }
+    true
+}
+
+fn named_children(node: Node) -> Vec<Node> {
+    // calling allocating a new cursor every time is less effecient, but juggling lifetimes also sucks.
+    node.named_children(&mut node.walk()).collect()
+}
+
+
+
+fn type_check(source_bytes: &Vec<u8>, tree: Tree) -> Result<ExprT, TypeError> {
+    _type_check(source_bytes, tree.root_node())
+}
+
+fn _type_check(source: &Vec<u8>, node: Node) -> Result<ExprT, TypeError> {
+    // let kind = node.kind();
+    // match kind {
+    //     "source_file" => {
+    //         let x: Result<Vec<ExprT>, TypeError> = 
+    //         named_children(node)
+    //                 .into_iter()
+    //                 .map(|child| _type_check(source, child))
+    //                 .collect();
+
+    //         match x {
+    //             Ok(children) => Ok(ExprT::RootT(children)),
+    //             Err(_)       => Err(TypeError::BadSourceExtraction),
+    //         }
+    //     },
+
+    //     "lit_string" => {
+    //         match text_from_node(&source, &node) {
+    //             Ok(s)  => Ok(ExprT::StringT(strip_first_and_last(s))),
+    //             Err(_) => Err(TypeError::BadSourceExtraction),
+    //         }
+    //     },
+
+    //     "bool" => {
+    //         match text_from_node(&source, &node) {
+    //             Ok("True")  => Ok(ExprT::BoolT(true)),
+    //             Ok("False") => Ok(ExprT::BoolT(false)),
+    //             Ok(s)       => Err(TypeError::BadBoolean(s.to_owned())),
+    //             Err(_)      => Err(TypeError::BadSourceExtraction),
+    //         }
+    //     },
+
+    //     // Throw away names. 
+    //     // In the step before getting here, names will be checked for correctness.
+    //     "kwarg" => {
+    //         let value = child_by_field_name(&node, "value")?;
+    //         match value.kind() {
+    //             "fn_call" => Err(TypeError::BadAssignment("kwarg".to_owned(), "fn_call".to_owned())),
+    //             // No intermediary kwarg type. Just return the underlying value.
+    //             _         => _type_check(source, value),
+    //         }
+    //     },
+
+    //     "dict" => {
+    //         let mut dict = HashMap::new();
+    //         for pair in named_children(node) {
+    //             let key_node = child_by_field_name(&pair, "key")?;
+    //             let key = strip_first_and_last(text_from_node(&source, &key_node)?);
+    //             let value_node = child_by_field_name(&pair, "key")?;
+    //             let value = _type_check(source, value_node)?;
+
+    //             dict.insert(key, value);
+    //         }
+    //         // TODO stub
+    //         Ok(ExprT::DictT(dict))
+    //     },
+
+    //     "list" => {
+    //         let mut list = vec![];
+    //         for elem in named_children(node) {
+    //             if elem.kind() == "fn_call" {
+    //                 return Err(TypeError::BadAssignment("list element".to_owned(), "fn_call".to_owned()))
+    //             }
+    //             let e = _type_check(source, elem)?;
+    //             list.push(e);
+    //         }
+    //         Ok(ExprT::ListT(list))
+    //     },
+
+        // "fn_call" => {
+        //     let name = text_from_node(source_bytes, child_by_field_name(&node, "fn_name")?)?;
+        //     let arg_list = child_by_field_name(node, "argument_list")
+        //     let arg_count = arg_list.named_child_count();
+        //     let args = named_children(arg_list)?;
+        //     if not kwargs_last(args) {
+        //         return Err(TypeError::KwargsAreNotLast)
+        //     }
+
+        // },
+
+    // }
+}
+
+pub fn extract_from_source(source: &str) -> Result<Extraction, ParseError> {
     let source_bytes: Vec<u8> = source.as_bytes().to_vec();
     let mut parser = tree_sitter::Parser::new();
     parser.set_language(tree_sitter_jinja2::language()).expect("Error loading jinja2 grammar");
     let tree = parser.parse(source, None).unwrap();
 
-    type_check(&source_bytes, tree)?;
+    // TODO compose these
+    map_err(to_ast(&source_bytes, tree.root_node()), ParseError::SourceE)?;
+    map_err(type_check(&source_bytes, tree), ParseError::TypeE)?;
 
     Ok(Extraction {
         refs: vec![],
