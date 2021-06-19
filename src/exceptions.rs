94702ccc1fae4965f4f0ac32cd08731b7e0aa273
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-06-19 17:18:50 -0400

    use thiserror to remove exception boilerplate

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -1,90 +1,53 @@
-use std::fmt;
-use std::fmt::Display;
 use std::str::Utf8Error;
-
-// TODO change Errors to contain &str not String?
+use thiserror::Error;
 
 
 // Top-level error type in the hierarchy
-#[derive(Debug, Clone, Eq, PartialEq)]
+#[derive(Error, Debug, Clone, Eq, PartialEq)]
 pub enum ParseError {
-    SourceE(SourceError),
-    TypeE(TypeError),
+    #[error("Source Error: ")]
+    SourceE(#[from] SourceError),
+    #[error("Type Error: ")]
+    TypeE(#[from] TypeError),
 }
 
-impl Display for ParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            ParseError::SourceE(e) => Display::fmt(e, f),
-            ParseError::TypeE(e) => Display::fmt(e, f),
-        }
-    }
-}
-
-// TODO use crate `thiserror` to simplify boilerplate
-#[derive(Debug, Clone, Eq, PartialEq)]
+// Errors from tree-sitter -> ExprU
+#[derive(Error, Debug, Clone, Eq, PartialEq)]
 pub enum SourceError {
+    #[error("Tree Sitter Error")]
     TreeSitterError,
-    Utf8Err(Utf8Error),
+    #[error("Utf8 Error: ")]
+    Utf8Err(#[from] Utf8Error),
+    #[error("Unknown Boolean value: {0}")]
     BadBoolean(String),
+    #[error("Unknown node type: {0}")]
     UnknownNodeType(String),
+    #[error("{0} is missing the required value {1}")]
     MissingValue(String, String),
+    #[error("Parse Failure")]
     ParseFailure,
 }
 
-impl Display for SourceError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            SourceError::TreeSitterError =>
-                write!(f, "tree-sitter found an error"),
-            SourceError::Utf8Err(e) =>
-                e.fmt(f),
-            SourceError::BadBoolean(s) =>
-                write!(f, "Unknown Boolean value: {}", s),
-            SourceError::UnknownNodeType(s) =>
-                write!(f, "Unknown node type: {}", s),
-            SourceError::MissingValue(outer, inner) =>
-                write!(f, "{} is missing the required value {}", outer, inner),
-            SourceError::ParseFailure =>
-                write!(f, "tree-sitter failed to parse the source"),
-        }
-    }
-}
-
-#[derive(Debug, Clone, Eq, PartialEq)]
+// errors from ExprU -> ExprT
+#[derive(Error, Debug, Clone, Eq, PartialEq)]
 pub enum TypeError {
+    #[error("{0} cannot be assigned a {1}")]
     BadAssignment(String, String),
+    #[error("Keyword arguments must come at the end of the argument list.")]
     KwargsAreNotLast,
     // TODO expected should be a Vec<usize> (e.g. - ref takes 1 or 2 args)
+    #[error("Expected {expected:?} arguments. Found {found:?}.")]
     ArgumentMismatch { expected: String, found: usize },
     // use ExprU::type_string() when creating this exception to get the right human readable name for each type.
     // TODO add a new type `ExprType` that maps 1:1 values - types. Do string conversion after on that value.
+    #[error("Expected {expected:?}. Got {got:?} ")]
     TypeMismatch { expected: String, got: String },
+    #[error("Found unrecognized function named {0}.")]
     UnrecognizedFunction(String),
+    #[error("Found unexpected keyword argument {0}.")]
     UnexpectedKwarg(String),
+    #[error("Excluded keyword argument found: {0}.")]
     ExcludedKwarg(String),
+    #[error("Config value cannot be of the type {0}.")]
     UnsupportedConfigValue(String),
-}
-
-impl Display for TypeError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            TypeError::BadAssignment(outer, inner) =>
-                write!(f, "{} cannot be assigned a {}", outer, inner),
-            TypeError::KwargsAreNotLast =>
-                write!(f, "Keyword arguments must come at the end of the argument list."),
-            TypeError::ArgumentMismatch { expected, found } =>
-                write!(f, "Expected {} arguments. Found {}.", expected, found),
-            TypeError::TypeMismatch { expected, got } =>
-                write!(f, "Expected {}. Got {} ", expected, got),
-            TypeError::UnrecognizedFunction(name) =>
-                write!(f, "Found unrecognized function named {}.", name),
-            TypeError::UnexpectedKwarg(key) =>
-                write!(f, "Found unexpected keyword argument {}.", key),
-            TypeError::ExcludedKwarg(key) =>
-                write!(f, "Excluded keyword argument found: {}.", key),
-            TypeError::UnsupportedConfigValue(value_type) =>
-                write!(f, "Config value cannot be of the type {}.", value_type),
-        }
-    }
 }
