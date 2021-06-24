244bb53508ec9c7e08b1ca9e690b84d331b4b4e8
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2021-06-24 16:47:00 -0400

    test exception message rendering

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -6,9 +6,9 @@ use thiserror::Error;
 // Top-level error type in the hierarchy
 #[derive(Error, Debug, Clone, Eq, PartialEq)]
 pub enum ParseError {
-    #[error("Source Error: ")]
+    #[error("Source Error: {0}")]
     SourceE(#[from] SourceError),
-    #[error("Type Error: ")]
+    #[error("Type Error: {0}")]
     TypeE(#[from] TypeError),
 }
 
@@ -17,7 +17,7 @@ pub enum ParseError {
 pub enum SourceError {
     #[error("Tree Sitter Error")]
     TreeSitterError,
-    #[error("Utf8 Error: ")]
+    #[error("Utf8 Error: {0}")]
     Utf8Err(#[from] Utf8Error),
     #[error("Unknown Boolean value: {0}")]
     BadBoolean(String),
@@ -38,7 +38,7 @@ pub enum TypeError {
     KwargsAreNotLast,
     #[error("Expected {} arguments. Found {found}.", expected_arity(expected))]
     ArgumentMismatch { expected: Vec<usize>, found: usize },
-    #[error("Expected {}. Got {}.", .expected.to_string(), .expected.to_string())]
+    #[error("Expected {}. Got {}.", .expected.to_string(), .got.to_string())]
     TypeMismatch { expected: ExprType, got: ExprType },
     #[error("Found unrecognized function named {0}.")]
     UnrecognizedFunction(String),
@@ -75,11 +75,82 @@ fn expected_arity(expected: &Vec<usize>) -> String {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    #[test]
+    fn exception_messages_render_as_expected() {
+        use ParseError::*;
+        use SourceError::*;
+        use TypeError::*;
+        use ExprType::*;
+
+        let examples = [
+            // TODO: "syntax error in source"
+            (
+                SourceE(TreeSitterError), 
+                "Source Error: Tree Sitter Error"
+            ),
+            // excluding Utf8Err for now,
+            (
+                SourceE(BadBoolean("TrueFalse".to_owned())), 
+                "Source Error: Unknown Boolean value: TrueFalse"
+            ),
+            (
+                SourceE(UnknownNodeType("not_a_node".to_owned())),
+                "Source Error: Unknown node type: not_a_node"
+            ),
+            (
+                SourceE(MissingValue("kwarg".to_owned(), "key".to_owned())),
+                "Source Error: kwarg is missing the required value key"
+            ),
+            (
+                SourceE(ParseFailure),
+                "Source Error: Parse Failure"
+            ),
+            (
+                // TODO this exception shouldn't be taking String args
+                TypeE(BadAssignment("kwarg".to_owned(), "fn_call".to_owned())),
+                "Type Error: kwarg cannot be assigned a fn_call"
+            ),
+            (
+                TypeE(KwargsAreNotLast),
+                "Type Error: Keyword arguments must come at the end of the argument list."
+            ),
+            (
+                TypeE(ArgumentMismatch { expected: vec![1, 2], found: 0 }),
+                "Type Error: Expected 1 to 2 arguments. Found 0."
+            ),
+            (
+                TypeE(TypeMismatch { expected: Kwarg, got: String }),
+                "Type Error: Expected kwarg. Got string."
+            ),
+            (
+                TypeE(UnrecognizedFunction("refsourceconfig".to_owned())),
+                "Type Error: Found unrecognized function named refsourceconfig."
+            ),
+            (
+                TypeE(UnexpectedKwarg("boop".to_owned())),
+                "Type Error: Found unexpected keyword argument boop."
+            ),
+            (
+                TypeE(ExcludedKwarg("pre-hook".to_owned())),
+                "Type Error: Excluded keyword argument found: pre-hook."
+            ),
+            (
+                TypeE(UnsupportedConfigValue(FnCall)),
+                "Type Error: Config value cannot be of the type fn_call."
+            ),
+        ];
+
+        // actually make the assertions
+        for (e, msg) in &examples {
+            assert_eq!(format!("{}", e), msg.to_owned())
+        }
+    }
 
     #[test]
     // if you change this test, change the comments on the function to match
     fn test_expected_arity() {
-        use super::expected_arity;
         assert_eq!(expected_arity(&vec![1,4,3,2]), "1 to 4");
         assert_eq!(expected_arity(&vec![1,4]), "1 to 4");
         assert_eq!(expected_arity(&vec![3]), "3");
